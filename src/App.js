60da8602a383fa65dfac5ec3d868738d80b3644e
Ruella-De-Vil <159031808+Ruella-De-Vil@users.noreply.github.com>
2024-06-21 10:29:38 +0200

    initial css

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import './App.css';
 import React from 'react';
-import Weather from "./Weather.js";
+import Weather from './weather.js'
 
 function App() {
   return (
     <div className="App">
       <div className="container">
-        <header>
-          <p className="pt-4">search engine</p>
-        </header>
-        <main>
           <Weather />
-        </main>
       </div>
     </div>
   );
--- src/Weather.js
+++ src/Weather.js
@@ -3,17 +3,28 @@ import "./Weather.css";
 
 export default function Weather() {
     return (
-        <div className="weather">
-            <h1>Durban</h1>
-            <p>Tuesday</p>
-            <p>10:00</p>
-            <h4>Partly cloudy</h4>
-            <h2>24°C
-            <img href="" alt="icon"></img>
+        <div className="Weather mt-3 pb-1 ps-4 pe-4" >
+            <div  className="search-engine pt-4 pb-3">
+                <form>
+                    <input className="search-bar" type="text" placeholder="Enter City..." />
+                    <input className="search-btn" type="submit" value="Search" />
+                </form>
+            </div >
+            <h1 className="city">Durban</h1>
+            <p className="day-and-time">Tuesday, 10:00</p>
+            <div className="row">
+            <div className="current-condition col">
+             <p>Partly cloudy</p>
+             <h2 className="current-temp">24°C
+            <img src="https://ssl.gstatic.com/onebox/weather/64/sunny.png" alt="icon"></img>
             </h2>
+            </div>
+            <div className="w-h-f col">
             <p>Wind: 10km/h</p>
             <p>Humidity: 60%</p>
             <p>Feels Like: 24°C</p>
+            </div>
+            </div>
         </div>
     )
 }
